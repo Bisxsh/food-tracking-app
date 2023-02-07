b15668bad88845dbc18ed619152b8bf26290ae2f
yoshiaki-shiokawa <ys2445@bath.ac.uk>
2023-02-07 17:28:35 +0000

    Added the project with the bottom navigation bar

---
+++ App.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
+
+import {Home} from './src/screens/Home';
+import {Recipe} from './src/screens/Recipe';
+import {Profile} from './src/screens/Profile';
+
+const Tab = createBottomTabNavigator();
+
+function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: '#27C179',
+        headerShown: false,
+      }}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Recipe"
+          component={Recipe}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="food" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="account" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+export default App;
---
+++ src/screens/Profile.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, StatusBar, Text, View} from 'react-native';
+
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+
+export function Profile(): JSX.Element {
+  const isDarkMode = false;
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <View
+      style={{
+        backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>This is Profile page</Text>
+    </View>
+  );
+}
