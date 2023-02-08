52d4cc82fd8d5a310b0ccb6b2f26e1893fe59eff
Bisesh <bisesh.sitaula@gmail.com>
2023-02-08 13:53:48 +0000

    ⌛ Created HomeFilters enum

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,25 +1,15 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import { Colors, Header } from "react-native/Libraries/NewAppScreen";
-import Checkbox from "../../components/Checkbox";
-import CustomSearchBar from "../../components/CustomSearchBar";
-import SortButton from "../../components/SortButton";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 import HomeMenu from "./HomeMenu";
+import { HomeFilters } from "./HomeFilters";
 
 export function Home(): JSX.Element {
   const isDarkMode = false;
   const [ingredientsSearch, setIngredientsSearch] = useState("");
   const [selectedFilter, setSelectedFilter] = useState(0);
   const [checked, setChecked] = useState(false);
-
-  //TODO replace with obj
-  const filters = [
-    "Expiry Date: Low to High",
-    "Expiry Date: High to Low",
-    "Quantity: Low to High",
-    "Quantity: High to Low",
-  ];
 
   return (
     <View
@@ -31,7 +21,7 @@ export function Home(): JSX.Element {
       }}
     >
       <HomeMenu
-        filters={filters}
+        filters={HomeFilters}
         ingredientsSearch={ingredientsSearch}
         selectedFilter={selectedFilter}
         setIngredientsSearch={setIngredientsSearch}
