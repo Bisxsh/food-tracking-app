ce5e17268f24dfefdde8365b236bb374e09e7bdf
Bisesh <bisesh.sitaula@gmail.com>
2023-02-22 20:31:09 +0000

    🏓 Added padding to setting options

--- src/screens/Profile/Screens/Setting.tsx
+++ src/screens/Profile/Screens/Setting.tsx
@@ -1,15 +1,28 @@
-import { MaterialIcons } from '@expo/vector-icons';
-import React, {useContext, useEffect, useState} from 'react';
-import {ScrollView, StyleSheet, Switch, Text, TouchableOpacity, View} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { MaterialIcons } from "@expo/vector-icons";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Switch,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
-import { COLOURS, FONT_SIZES, ICON_SIZES, RADIUS, SPACING } from '../../../util/GlobalStyles';
-import { User, UserContext } from '../../../backends/User';
-import * as DB from '../../../backends/Database';
-import { UserSetting } from '../../../backends/UserSetting';
-import { ScreenProp, StackParams } from '../ProfileNavigator';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import {
+  COLOURS,
+  FONT_SIZES,
+  ICON_SIZES,
+  RADIUS,
+  SPACING,
+} from "../../../util/GlobalStyles";
+import { User, UserContext } from "../../../backends/User";
+import * as DB from "../../../backends/Database";
+import { UserSetting } from "../../../backends/UserSetting";
+import { ScreenProp, StackParams } from "../ProfileNavigator";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 const HorizontalLine = (
   <View
@@ -21,38 +34,47 @@ const HorizontalLine = (
   />
 );
 
-const NavigateRow = (text:string, destination: keyof StackParams, navigation: NativeStackNavigationProp<StackParams, any, undefined>)=>{
+const NavigateRow = (
+  text: string,
+  destination: keyof StackParams,
+  navigation: NativeStackNavigationProp<StackParams, any, undefined>
+) => {
   return (
     <TouchableOpacity
       style={{
         flexDirection: "row",
         justifyContent: "space-between",
         alignSelf: "flex-start",
+        paddingTop: SPACING.small,
+        paddingBottom: SPACING.small,
       }}
-      onPress={async ()=>{
-        navigation.navigate(destination)
+      onPress={async () => {
+        navigation.navigate(destination);
       }}
     >
-      <Text style={{
+      <Text
+        style={{
           flex: 1,
           fontSize: FONT_SIZES.medium,
           alignSelf: "center",
           marginVertical: SPACING.small,
         }}
-      >{text}</Text>
-      <MaterialIcons 
-        name="arrow-forward-ios" 
-        color={COLOURS.black} 
-        size={ICON_SIZES.medium} 
-        style={{alignSelf: "center",}}
+      >
+        {text}
+      </Text>
+      <MaterialIcons
+        name="arrow-forward-ios"
+        color={COLOURS.black}
+        size={ICON_SIZES.medium}
+        style={{ alignSelf: "center" }}
       />
     </TouchableOpacity>
   );
-}
+};
 
-const SwitchRow = (text:string, key: keyof UserSetting)=>{
+const SwitchRow = (text: string, key: keyof UserSetting) => {
   const { user, setUser } = useContext(UserContext);
-  const [ value, setValue ] = useState<boolean>(user.setting[key] as boolean);
+  const [value, setValue] = useState<boolean>(user.setting[key] as boolean);
 
   return (
     <View
@@ -63,71 +85,77 @@ const SwitchRow = (text:string, key: keyof UserSetting)=>{
         alignSelf: "flex-start",
       }}
     >
-      <Text style={{
+      <Text
+        style={{
           flex: 1,
           fontSize: FONT_SIZES.medium,
           alignSelf: "center",
           marginVertical: SPACING.small,
         }}
-      >{text}</Text>
+      >
+        {text}
+      </Text>
       <Switch
         style={{
-          alignSelf: "center"
+          alignSelf: "center",
         }}
         value={value}
-        onValueChange={(value)=>{
-          (user.setting[key] as boolean) = value
-          setUser(user)
-          setValue(value)
-          DB.updateUser(user)
+        onValueChange={(value) => {
+          (user.setting[key] as boolean) = value;
+          setUser(user);
+          setValue(value);
+          DB.updateUser(user);
         }}
       />
     </View>
   );
-}
+};
 
-const TouchableRow = (text:string, func:Function)=>{
+const TouchableRow = (text: string, func: Function) => {
   return (
     <TouchableOpacity
       style={{
         flexDirection: "row",
         justifyContent: "space-between",
         alignSelf: "flex-start",
+        paddingTop: SPACING.small,
+        paddingBottom: SPACING.small,
       }}
       onPress={func()}
     >
-      <Text style={{
+      <Text
+        style={{
           flex: 1,
           fontSize: FONT_SIZES.medium,
           alignSelf: "center",
           marginVertical: SPACING.small,
           color: COLOURS.textTouchable,
         }}
-      >{text}</Text>
+      >
+        {text}
+      </Text>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-export function Setting({navigation}: ScreenProp): JSX.Element {
+export function Setting({ navigation }: ScreenProp): JSX.Element {
   const { user, setUser } = useContext(UserContext);
-  const [isDarkMode, setIsDarkMode] = useState<boolean>(user.setting.isDark())
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(user.setting.isDark());
 
-  useEffect(
-    ()=>{
-      const unsubscribe = navigation.addListener("focus", ()=>{
-        setIsDarkMode(user.setting.isDark())
-      })
-      return unsubscribe
-    }, 
-    [navigation]
-  ) 
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      setIsDarkMode(user.setting.isDark());
+    });
+    return unsubscribe;
+  }, [navigation]);
 
   return (
     <ScrollView
       style={{
         backgroundColor: isDarkMode ? Colors.darker : Colors.white,
         flex: 1,
-      }}>
+      }}
+    >
       <View style={styles.container}>
         {NavigateRow("Edit Account", "Account", navigation)}
       </View>
@@ -138,11 +166,9 @@ export function Setting({navigation}: ScreenProp): JSX.Element {
       </View>
       <View style={styles.container}>
         {SwitchRow("Debug Mode", "debug")}
-        {user.setting.debug && (
-          NavigateRow("Debug Window", "Debug", navigation)
-        )}
+        {user.setting.debug && NavigateRow("Debug Window", "Debug", navigation)}
         {HorizontalLine}
-        {TouchableRow("Reset", ()=>{})}
+        {TouchableRow("Reset", () => {})}
         {HorizontalLine}
         {NavigateRow("Help", "Help", navigation)}
         {HorizontalLine}
@@ -151,7 +177,6 @@ export function Setting({navigation}: ScreenProp): JSX.Element {
     </ScrollView>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
