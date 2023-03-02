e30b6733b6823f9fe65d684bb930cbe61d9fd23a
Bisesh <bisesh.sitaula@gmail.com>
2023-03-02 20:11:49 +0000

    🥓📜 Implemented meal ingredients list

--- src/components/RecipeIngredientList.tsx
+++ src/components/RecipeIngredientList.tsx
@@ -1,90 +1,252 @@
-import { StyleSheet, Text, TextInput, View, Image, ScrollView, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from "react-native";
 import React, { useContext, useEffect, useState } from "react";
-import { readAllIngredient } from "../backends/Database";
 import CustomSearchBar from "./CustomSearchBar";
-import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
-import { getRecipes } from '../util/GetRecipe';
 import { COLOURS, DROP_SHADOW, RADIUS, SPACING } from "../util/GlobalStyles";
-import IngredientsList from "./InstructionsList";
-
+import { UserDataContext } from "../classes/UserData";
+import {
+  Ingredient,
+  IngredientBuilder,
+  weightUnit,
+} from "../classes/IngredientClass";
+import Checkbox from "./Checkbox";
+import { getDaysUntilExpiry, getTimeLeft } from "../util/ExpiryCalc";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Modal from "react-native-modal/dist/modal";
 
 function RecipeIngredientList() {
-
-
-    useEffect(() => {
-        RecipeIngredientList();
-    }, []);
-
-    const [ingredientsSearch, setIngredientsSearch] = useState("");
-    const [ingredientList, setIngredientList] = useState<any[]>([]);
-
-
-    async function RecipeIngredientList() {
-        let ingredient = await readAllIngredient();
-        setIngredientList(ingredient);
-    }
+  const [ingredientsSearch, setIngredientsSearch] = useState("");
+  const { userData, setUserData } = useContext(UserDataContext);
+  const [ingredientList, setIngredientList] = useState<Ingredient[]>(
+    userData.storedIngredients
+  );
+  const [usedIngredientsList, setUsedIngredientsList] = useState<Ingredient[]>(
+    []
+  );
+  const [showModal, setShowModal] = useState(false);
+  const [ingredientBeingEdited, setIngredientBeingEdited] =
+    useState<IngredientBuilder | null>(null);
 
   return (
-    <><Text>Ingredients</Text>
-    
-    <View style={styles.container}>
-        <View style={{paddingTop:10}}>
-        <ScrollView >
-    <CustomSearchBar textHint={"Search Ingredient"} text={ingredientsSearch} setText={setIngredientsSearch} width={360} height={40} />
-    {
-        ingredientList.map((ingredient) => {
-          {console.log(ingredient)}
-            return (
-                <TouchableOpacity>
-                <View style={{padding:10}} key={ingredient}>
-                    <Text>{ingredient}</Text>
-                    <Image source={{uri: "https://d.newsweek.com/en/full/530445/12-23-lingscars-01.jpg?w=1600&h=1600&q=88&f=526c077afee8b191f79da78615ee562b"}} style={{width: 50, height: 50}} />
-                </View>
-                </TouchableOpacity>
-            )
-        })
-    }
-    </ScrollView>
-    </View>
-    </View>
+    <>
+      <Text>Ingredients</Text>
+
+      <View style={styles.container}>
+        <View style={{ paddingTop: 10 }}>
+          <ScrollView>
+            <CustomSearchBar
+              textHint={"Search Ingredient"}
+              text={ingredientsSearch}
+              setText={setIngredientsSearch}
+              width={360}
+              height={40}
+            />
+            {ingredientList
+              .filter((i) => i.quantity > 0)
+              .filter((i) => i.expiryDate > new Date())
+              .filter((i) => i.name.includes(ingredientsSearch))
+              .map((ingredient) => {
+                return (
+                  <View>
+                    <View
+                      style={styles.ingredientContainer}
+                      key={ingredient.name}
+                    >
+                      <Checkbox
+                        initialVal={usedIngredientsList.includes(ingredient)}
+                        onPress={(checked) => {
+                          if (checked) {
+                            setUsedIngredientsList([
+                              ...usedIngredientsList,
+                              ingredient,
+                            ]);
+                          } else {
+                            setUsedIngredientsList(
+                              usedIngredientsList.filter(
+                                (i) => i.name != ingredient.name
+                              )
+                            );
+                          }
+                        }}
+                      />
+                      {ingredient.imgSrc && (
+                        <Image
+                          source={{
+                            uri: ingredient.imgSrc,
+                          }}
+                          style={{
+                            width: 50,
+                            height: 50,
+                            marginRight: SPACING.small,
+                            borderRadius: RADIUS.tiny,
+                          }}
+                        />
+                      )}
+                      <View style={{ flexDirection: "column" }}>
+                        <Text>{ingredient.name}</Text>
+                        {getDaysUntilExpiry(ingredient) < 1 &&
+                        getDaysUntilExpiry(ingredient) > 0 ? (
+                          <View
+                            style={{
+                              flexDirection: "row",
+                              justifyContent: "center",
+                              alignItems: "center",
+                            }}
+                          >
+                            <MaterialCommunityIcons
+                              name="alert-outline"
+                              size={16}
+                              color={"#A04444"}
+                            />
+                            <Text style={{ color: "#A04444" }}>
+                              Expires in {getTimeLeft(ingredient)}
+                            </Text>
+                          </View>
+                        ) : (
+                          <></>
+                        )}
+                      </View>
+
+                      <View style={{ flex: 1 }} />
+                      <TouchableOpacity
+                        style={styles.amountContainer}
+                        onPress={() => {
+                          setShowModal(true);
+                          setIngredientBeingEdited(
+                            IngredientBuilder.fromIngredient(ingredient)
+                          );
+                        }}
+                      >
+                        <Text>
+                          {ingredient.servingSize}{" "}
+                          {ingredient.servingSizeType == weightUnit.grams
+                            ? "g"
+                            : "kg"}
+                        </Text>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                );
+              })}
+          </ScrollView>
+        </View>
+      </View>
+      <Modal
+        isVisible={showModal}
+        onBackdropPress={() => setShowModal(false)}
+        backdropOpacity={0.5}
+        animationIn="zoomIn"
+        animationOut="zoomOut"
+        style={StyleSheet.absoluteFill}
+      >
+        <View style={styles.modalContainer}>
+          <TextInput
+            placeholder={"Quantity"}
+            style={styles.textInput}
+            onChangeText={(text) => {
+              ingredientBeingEdited?.setServingSize(parseInt(text) || 0);
+            }}
+          />
+          <MaterialCommunityIcons
+            name="arrow-right-thin"
+            style={styles.confirmButton}
+            size={SPACING.medium}
+            onPress={() => {
+              setShowModal(false);
+              setIngredientList(
+                ingredientList.map((i) =>
+                  i.name == ingredientBeingEdited?.getName()
+                    ? ingredientBeingEdited.build()
+                    : i
+                )
+              );
+              setIngredientBeingEdited(null);
+              //TODO implement
+            }}
+          />
+        </View>
+      </Modal>
     </>
-  )
+  );
 }
-export default RecipeIngredientList
+export default RecipeIngredientList;
 
 const styles = StyleSheet.create({
-    container: {
-      height: 250,
-      flexDirection: "column",
-      backgroundColor: COLOURS.white,
-      borderWidth: 2,
-      borderColor: COLOURS.grey,
-      borderRadius: 10,
-      alignItems: "flex-start",
-      justifyContent: "space-around",
-    },
-  
-    foodImage: {
-      borderRadius: RADIUS.small,
-      width: 124, 
-      height: 124,
-      marginRight: 'auto',
-      marginBottom: 'auto',
-    },
-  
-    textHeading: {
-      left: '35%',
-      bottom: "35%",
-      fontSize: 16,
-    },
-  
-    textSmall: {
-      left: '40%',
-      bottom: "40%",
-      fontSize: 14,
-      // position: 'absolute',
-    },
-  
-  });
-  
-  
+  container: {
+    height: 250,
+    flexDirection: "column",
+    backgroundColor: COLOURS.white,
+    borderWidth: 2,
+    borderColor: COLOURS.grey,
+    borderRadius: 10,
+    alignItems: "flex-start",
+    justifyContent: "space-around",
+  },
+
+  foodImage: {
+    borderRadius: RADIUS.small,
+    width: 124,
+    height: 124,
+    marginRight: "auto",
+    marginBottom: "auto",
+  },
+
+  textHeading: {
+    left: "35%",
+    bottom: "35%",
+    fontSize: 16,
+  },
+
+  textSmall: {
+    left: "40%",
+    bottom: "40%",
+    fontSize: 14,
+    // position: 'absolute',
+  },
+
+  ingredientContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: SPACING.tiny,
+    paddingHorizontal: SPACING.small,
+  },
+
+  amountContainer: {
+    marginRight: SPACING.small,
+    borderRadius: RADIUS.tiny,
+    borderColor: COLOURS.black,
+    borderWidth: 1,
+    padding: SPACING.small,
+    minWidth: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  modalContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: COLOURS.white,
+    padding: SPACING.medium,
+    borderRadius: RADIUS.standard,
+    ...DROP_SHADOW,
+  },
+
+  textInput: {
+    minWidth: 200,
+    marginLeft: SPACING.small,
+  },
+
+  confirmButton: {
+    borderRadius: RADIUS.circle,
+    backgroundColor: COLOURS.grey,
+    padding: SPACING.small,
+  },
+});
