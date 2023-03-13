67a04fd255cd0240204b57af0965e68878262ea9
Bisesh <bisesh.sitaula@gmail.com>
2023-03-13 17:22:12 +0000

    💭 Improved sort menu placement

--- src/components/SortButton.tsx
+++ src/components/SortButton.tsx
@@ -1,10 +1,4 @@
-import {
-  Animated,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { Animated, StyleSheet, Text } from "react-native";
 import React, { useEffect, useRef, useState } from "react";
 import { Octicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import {
@@ -15,6 +9,12 @@ import {
   SPACING,
 } from "../util/GlobalStyles";
 import Modal from "react-native-modal/dist/modal";
+import {
+  Menu,
+  MenuOption,
+  MenuOptions,
+  MenuTrigger,
+} from "react-native-popup-menu";
 
 type Props = {
   options: any[];
@@ -43,73 +43,142 @@ const SortButton = (props: Props) => {
     }).start();
   }, [showModal]);
 
+  const optionsStyles = {
+    optionsContainer: {
+      paddingTop: SPACING.medium,
+      paddingBottom: SPACING.medium,
+      backgroundColor: COLOURS.white,
+      width: props.width ? props.width : "auto",
+      borderRadius: RADIUS.standard,
+      ...DROP_SHADOW,
+    },
+  };
+
   return (
-    <View
-      style={{
-        position: "relative",
-        marginRight: props.removeMargin ? 0 : SPACING.small,
-      }}
-    >
-      <TouchableOpacity
-        style={styles(props).button}
-        onPress={() => setShowModal(true)}
-      >
+    <Menu style={{ marginRight: props.removeMargin ? 0 : SPACING.small }}>
+      <MenuTrigger style={styles(props).button}>
         <Octicons
           name="arrow-switch"
           size={24}
           color="black"
           style={{ transform: [{ rotate: "90deg" }] }}
         />
-      </TouchableOpacity>
-      <Modal
-        isVisible={showModal}
-        onBackdropPress={() => setShowModal(false)}
-        backdropOpacity={0}
-        animationIn="fadeInDown"
-        animationOut="fadeOutUp"
-        style={{
-          position: "absolute",
-          //TODO change to work with device for presentation
-          top: 50,
-          right: 60,
-        }}
-      >
-        <Animated.View
-          style={[
-            styles(props).modal,
-            { opacity: fadeAnim, translateY: transitionAnim },
-          ]}
-        >
-          <Text style={styles(props).modalHeading}>Sort By</Text>
-          {props.options?.map((option, index) => {
-            return (
-              <TouchableOpacity
-                key={`filter-${index}`}
-                style={{
-                  ...styles(props).modalOption,
-                  backgroundColor:
-                    props.selectedOption === index
-                      ? COLOURS.grey
-                      : COLOURS.white,
-                }}
-                onPress={() => {
-                  props.setSelectedOption(index);
-                  setShowModal(false);
-                }}
-              >
-                <MaterialCommunityIcons
-                  name={index % 2 == 0 ? "arrow-up-thin" : "arrow-down-thin"}
-                  size={24}
-                  color="black"
-                />
-                <Text>{option}</Text>
-              </TouchableOpacity>
-            );
-          })}
-        </Animated.View>
-      </Modal>
-    </View>
+      </MenuTrigger>
+      <MenuOptions customStyles={optionsStyles}>
+        <Text style={styles(props).modalHeading}>Sort By</Text>
+        {props.options?.map((option, index) => {
+          return (
+            <MenuOption
+              key={`filter-${index}`}
+              style={{
+                ...styles(props).modalOption,
+                backgroundColor:
+                  props.selectedOption === index ? COLOURS.grey : COLOURS.white,
+              }}
+              onSelect={() => {
+                props.setSelectedOption(index);
+                setShowModal(false);
+              }}
+            >
+              <MaterialCommunityIcons
+                name={index % 2 == 0 ? "arrow-up-thin" : "arrow-down-thin"}
+                size={24}
+                color="black"
+              />
+              <Text>{option}</Text>
+            </MenuOption>
+          );
+        })}
+      </MenuOptions>
+    </Menu>
   );
+  //   <View
+  //     style={{
+  //       position: "relative",
+  //       marginRight: props.removeMargin ? 0 : SPACING.small,
+  //     }}
+  //   >
+  //     <TouchableOpacity
+  //       style={styles(props).button}
+  //       onPress={() => setShowModal(true)}
+  //     >
+  //       <Octicons
+  //         name="arrow-switch"
+  //         size={24}
+  //         color="black"
+  //         style={{ transform: [{ rotate: "90deg" }] }}
+  //       />
+  //     </TouchableOpacity>
+  //     <Menu>
+  //       <MenuTrigger>
+  //       <Octicons
+  //         name="arrow-switch"
+  //         size={24}
+  //         color="black"
+  //         style={{ transform: [{ rotate: "90deg" }] }}
+  //       />
+  //       </MenuTrigger>
+  //       <MenuOptions>
+  //         <MenuOption onSelect={() => alert(`Save`)} text="Save" />
+  //         <MenuOption onSelect={() => alert(`Delete`)}>
+  //           <Text style={{ color: "red" }}>Delete</Text>
+  //         </MenuOption>
+  //         <MenuOption
+  //           onSelect={() => alert(`Not called`)}
+  //           disabled={true}
+  //           text="Disabled"
+  //         />
+  //       </MenuOptions>
+  //     </Menu>
+  //     <Modal
+  //       isVisible={showModal}
+  //       onBackdropPress={() => setShowModal(false)}
+  //       backdropOpacity={0}
+  //       animationIn="fadeInDown"
+  //       animationOut="fadeOutUp"
+  //       style={{
+  //         position: "absolute",
+  //         //TODO change to work with device for presentation
+  //         top: 50,
+  //         right: 60,
+  //       }}
+  //     >
+  //       <Animated.View
+  //         style={[
+  //           styles(props).modal,
+  //           { opacity: fadeAnim, translateY: transitionAnim },
+  //         ]}
+  //       >
+  //         <Text style={styles(props).modalHeading}>Sort By</Text>
+  //         {props.options?.map((option, index) => {
+  //           return (
+  //             <TouchableOpacity
+  //               key={`filter-${index}`}
+  //               style={{
+  //                 ...styles(props).modalOption,
+  //                 backgroundColor:
+  //                   props.selectedOption === index
+  //                     ? COLOURS.grey
+  //                     : COLOURS.white,
+  //               }}
+  //               onPress={() => {
+  //                 props.setSelectedOption(index);
+  //                 setShowModal(false);
+  //               }}
+  //             >
+  //               <MaterialCommunityIcons
+  //                 name={index % 2 == 0 ? "arrow-up-thin" : "arrow-down-thin"}
+  //                 size={24}
+  //                 color="black"
+  //               />
+  //               <Text>{option}</Text>
+  //             </TouchableOpacity>
+  //           );
+  //         })}
+  //       </Animated.View>
+  //     </Modal>
+  //   </View>
+  // );
 };
 
 export default SortButton;
